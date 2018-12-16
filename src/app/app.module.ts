dc4c59a66b68da1b6f335f746851888da2306829
majczel23000 <michal.razny23@gmail.com>
2018-12-16 13:46:32 +0100

    add(firebase) - add firebase install and config to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms'
 import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from 'angularfire2';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDPume-Zn_4HUm10rQSmsGld3Ure2h6nDU",
+  authDomain: "coffeeapp-mr.firebaseapp.com",
+  databaseURL: "https://coffeeapp-mr.firebaseio.com",
+  projectId: "coffeeapp-mr",
+  storageBucket: "coffeeapp-mr.appspot.com",
+  messagingSenderId: "888855459317"
+};
 
 @NgModule({
   declarations: [
@@ -18,7 +27,8 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
